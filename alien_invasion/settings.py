47cffcc6330ead88670d2d9ad2fcd70f92f0a747
Alan Reji <alanreji.2000@gmail.com>
2022-09-01 00:01:26 +0530

    adding changes for alien_invasion

---
+++ alien_invasion/settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (100,100,100)
+        self.ship_speed = 1.5
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (60,60,60)
+        self.bullets_allowed = 3
+
+        self.alien_speed = .5
+        self.drop_speed = 1
+        self.fleet_direction = 1
